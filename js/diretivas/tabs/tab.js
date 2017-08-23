e9b8b15ed6054308be9143b699a8262c41face7d
flavio <flavio@localhost>
2017-08-23 20:44:33 -0300

    diretivas ta rodando

--- js/diretivas/tabs/tab.js
+++ js/diretivas/tabs/tab.js
@@ -6,9 +6,34 @@ angular.module('target').directive('tab', function () {
 		transclude: true,
 		templateUrl: 'js/diretivas/tabs/tab.html',
 		require: '^tabs',
+		scope:{
+			titulo:'@'
+		},
 		link: function(scope, element,attrs,tabsCtrl){
+			console.log(scope);
 			tabsCtrl.adicionaTab(scope);
 		}
 		
 	};
 });
+
+//.directive('', ['', function(){
+	// Runs during compile
+//	return {
+		// name: '',
+		// priority: 1,
+		// terminal: true,
+		// scope: {}, // {} = isolate, true = child, false/undefined = no change
+		// controller: function($scope, $element, $attrs, $transclude) {},
+		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		// restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+		// template: '',
+		// templateUrl: '',
+		// replace: true,
+		// transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+		//link: function($scope, iElm, iAttrs, controller) {
+			
+		//}
+	//};
+//}]);
