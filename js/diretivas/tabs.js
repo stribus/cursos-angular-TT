ee3d7f9b754dd1d145eac5bbcd52b137e4d65904
flavio <flavio@localhost>
2017-08-23 19:27:58 -0300

    diretivas

---
+++ js/diretivas/tabs.js
@@ -0,0 +1,11 @@
+angular.module('target').directive('tabs', function(){
+	
+	return {
+		
+		 restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+		 template: 'AQUI vai as tabs',
+		link: function(scope, element, attrs) {
+			
+		}
+	};
+});
