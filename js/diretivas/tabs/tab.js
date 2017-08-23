6c9628a6de6505d70ad1c7601a113c682d4fa672
flavio <4411132+stribus@users.noreply.github.com>
2017-08-23 20:36:47 -0300

    diretivas

--- js/diretivas/tabs/tab.js
+++ js/diretivas/tabs/tab.js
@@ -5,12 +5,10 @@ angular.module('target').directive('tab', function () {
 		restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
 		transclude: true,
 		templateUrl: 'js/diretivas/tabs/tab.html',
-		bindToController:true,
-		controllerAs:'tabs',
-		controller: function(){
-			var self = this;
-			self.tabs = [];
-			
+		require: '^tabs',
+		link: function(scope, element,attrs,tabsCtrl){
+			tabsCtrl.adicionaTab(scope);
 		}
+		
 	};
 });
--- js/diretivas/tabs/tabs.js
+++ js/diretivas/tabs/tabs.js
@@ -4,7 +4,16 @@ angular.module('target').directive('tabs', function () {
 
 		restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
 		transclude: true,
-		templateUrl: 'js/diretivas/tabs/tabs.html'
+		templateUrl: 'js/diretivas/tabs/tabs.html',
+		bindToController:true,
+		controllerAs:'tabs',
+		controller: function(){
+			var self = this;
+			self.tabs = [];
+			self.adicionaTab = function(tab){
+				self.tabs.push(tab);
+			};
+		}
 
 	};
 });
