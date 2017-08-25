d20ea8cce8da86243347d6d7f75cc63b7020c653
flavio <4411132+stribus@users.noreply.github.com>
2017-08-24 22:22:16 -0300

    criaqndo autenticacao

--- projeto/js/controllers/cabecalhoCTRL.js
+++ projeto/js/controllers/cabecalhoCTRL.js
@@ -1,6 +1,19 @@
 var app = angular.module('target')
 
-app.controller('CabecalhoCtrl', ['autenticacao',function(autenticacao){
+app.controller('CabecalhoCtrl', ['autenticacao','$rootScope',function(autenticacao,$rootScope){
 	var self = this;
+	self.usuarioLogado = false;
+	self.entrar= function () {
+		autenticacao.entrar()
+	}
+
+	self.sair = function () {
+		autenticacao.sair()
+	}
+
+	$rootScope.$on('usuarioLogado',function (event,value) {
+		self.usuarioLogado = value;
+		
+	})
 	
 }])
