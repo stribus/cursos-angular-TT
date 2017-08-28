6a14b68431bea953ae21db2ee00baa67602240d2
flavio <flavio@localhost>
2017-08-28 19:55:34 -0300

    criando rotas

--- projeto/js/app.js
+++ projeto/js/app.js
@@ -1,1 +1,10 @@
-var app = angular.module('target', ['ngMask']);
+var app = angular.module('target', ['ngMask','ngRoute']);
+
+app.config(['$routeProvider',function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl:'views/lista.html'
+	})
+	.otherwise({
+		redirectTo:'/'
+	});
+}])
